7e682bd9495b1b75a7a188272754abe1745d7ea6
Dhyagheshan VR <52463303+Dhyagheshan@users.noreply.github.com>
2020-10-09 13:43:12 +0530

    Update UserController.java

--- src/main/java/com/sb/demo/controller/UserController.java
+++ src/main/java/com/sb/demo/controller/UserController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sb.demo.dao.UserRepo;
 import com.sb.demo.model.User;
 
-@Controller
-
+@RestController
 public class UserController {
 	
 	@Autowired
